e0bf01e7260770452e3f9f24dad21bf4958fe191
oscar anillo <oscarani14@gmail.com>
2022-09-28 12:24:27 -0500

    update heroComponent css, add mastercraft

--- src/Components/main-component.js
+++ src/Components/main-component.js
@@ -1,17 +1,9 @@
+import MastercraftComponent from "./mastercraft-component";
+
 export default function MainComponent(){
     return (
         <main className="main">
-            <div className="main-div">
-                <div className="main-innner-div">
-                    <img src="/images/logo-mastercraft.svg" alt="" className="img-master"/>
-                    <h1>Mastercraft Bamboo Monitor Riser</h1>
-                    <p>A beautifully handcrafted monitor stand to reduce neck and eye strain</p>
-                    <div className="main-inner-row">
-                        <button className="cyan-btn">Back this project</button>
-                        <button className="bookmark-btn"><img src="/images/icon-bookmark.svg" alt=""/></button>
-                    </div>
-                </div>
-            </div>
+            <MastercraftComponent />
         </main>
     )
 }
---
+++ src/Components/mastercraft-component.js
@@ -0,0 +1,15 @@
+export default function MastercraftComponent(){
+    return (
+        <div className="main-div">
+                <div className="main-innner-div">
+                    <img src="/images/logo-mastercraft.svg" alt="" className="img-master"/>
+                    <h1>Mastercraft Bamboo Monitor Riser</h1>
+                    <p>A beautifully handcrafted monitor stand to reduce neck and eye strain</p>
+                    <div className="main-inner-row">
+                        <button className="cyan-btn">Back this project</button>
+                        <button className="bookmark-btn"><img src="/images/icon-bookmark.svg" alt=""/></button>
+                    </div>
+                </div>
+            </div>
+    )
+}
