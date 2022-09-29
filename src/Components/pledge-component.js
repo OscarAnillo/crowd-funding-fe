8b080c6efbe5559105e1d09ef28af425d8de7750
oscar anillo <oscarani14@gmail.com>
2022-09-28 21:15:41 -0500

    Commit
    add className field to data.json
    add a bit of logic to button text in pledgeComponent

--- src/Components/pledge-component.js
+++ src/Components/pledge-component.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
+
 import data from "../data/data-products.json";
 
 export default function PledgeComponent(){
-    
+    const [test, setTest] = useState('act');
+
     return (
         <div>
             {data.map(item => (
-                <div key={item.name} className="pledge">
+                <div key={item.name} className={`${item.className}`}>
                     <h3>{item.name}</h3>
                     <h4>{item.pledge}</h4>
                     <p>{item.text}</p>
                     <h1>{item.quantity} <span>left</span></h1>
-                    <button>Select Reward</button>
+                    <button>{item.className === "pledge" ? "Select Reward" : "Out of Stock" }</button>
                 </div>
             ))}
         </div>
